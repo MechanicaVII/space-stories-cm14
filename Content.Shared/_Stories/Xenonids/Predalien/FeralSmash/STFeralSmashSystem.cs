9304ad5ce273bab511b5509fb329f9996b4cb6b5
Mechanica <skiletik666@gmail.com>
2026-08-07 05:34:06 +0500

    fix

--- Content.Shared/_Stories/Xenonids/Predalien/FeralSmash/STFeralSmashSystem.cs
+++ Content.Shared/_Stories/Xenonids/Predalien/FeralSmash/STFeralSmashSystem.cs
@@ -2,7 +2,9 @@ using Content.Shared._RMC14.Xenonids.Leap;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.Movement.Pulling.Systems;
+using Content.Shared.Weapons.Melee;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._Stories.Xenonids.Predalien.FeralSmash;
 
@@ -11,6 +13,7 @@ public sealed class STFeralSmashSystem : EntitySystem
     [Dependency] private readonly DamageableSystem _damageable = default!;
     [Dependency] private readonly PullingSystem _pulling = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
     {
@@ -22,6 +25,13 @@ public sealed class STFeralSmashSystem : EntitySystem
     private void OnFeralSmashLeapHit(Entity<STFeralSmashComponent> xeno, ref XenoLeapHitEvent args)
     {
         RemComp<XenoLeapingComponent>(xeno.Owner);
+
+        if (TryComp<MeleeWeaponComponent>(xeno.Owner, out var melee))
+        {
+            melee.NextAttack = _timing.CurTime;
+            Dirty(xeno.Owner, melee);
+        }
+
         _pulling.TryStartPull(xeno.Owner, args.Hit);
 
         var kills = TryComp<STPredalienComponent>(xeno.Owner, out var predalien)
